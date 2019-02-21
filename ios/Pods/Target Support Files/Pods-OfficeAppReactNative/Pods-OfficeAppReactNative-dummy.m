cf4ef083608a392dd0db627284b405b5ec47f50d
Devlin Duldulao <webmasterdevlin@gmail.com>
2019-02-21 15:25:54 -0500

    added updated MS AppCenter secret

---
+++ ios/Pods/Target Support Files/Pods-OfficeAppReactNative/Pods-OfficeAppReactNative-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OfficeAppReactNative : NSObject
+@end
+@implementation PodsDummy_Pods_OfficeAppReactNative
+@end
---
+++ ios/Pods/Target Support Files/Pods-OfficeAppReactNativeTests/Pods-OfficeAppReactNativeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OfficeAppReactNativeTests : NSObject
+@end
+@implementation PodsDummy_Pods_OfficeAppReactNativeTests
+@end
